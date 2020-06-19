bb2380c49f62ca40a992ae8df1ee7d5424a7c312
cookidgod84 <65999315+cookidgod84@users.noreply.github.com>
2020-06-19 15:33:53 +1200

    based off tomas-challenge
    the part of code that deals with the white line has been moved out of the main loop so that it only runs until there is no more white line, this saves a bit of processing power (has been tested, no problems). more importantly added a method that detects when the robot is stuck, this method calls another method which should get the robot unstuck. however this does not work atm

--- AVC_robot/robotchallenge.cpp
+++ AVC_robot/robotchallenge.cpp
@@ -1,231 +1,375 @@
 #include "robot.hpp"
+
 #include <algorithm>
+
 #include <functional>
+
 #include <cmath>
+
 #include <iostream>       // std::cout, std::endl
+
 #include <thread>         // std::this_thread::sleep_for
+
 #include <chrono>         // std::chrono::seconds
+
 #include <unistd.h>
+
+#include <cstdlib>
+
+#include <vector>
+
 
 
 struct RGB  // Define RGB structure
+
 {  
+
     int r; // R, G, and B values are 0-255
+
     int g;
+
     int b;
+
 };
 
+
+
 // Define HSV structure - Example: https://upload.wikimedia.org/wikipedia/commons/f/f2/HSV_color_solid_cone.png
+
 struct HSV 
+
 {  
+
     int H;  // Hue, 0 - 360 (what base colour this actually is (sees in the dark!))
+
     float S;  // Saturation, 0-1 
+
     float V; // Value, 0-1
+
 };    
 
+
+
 HSV returnHSV(int r, int g, int b) // RGB to HSV function I created by adapting the math's algorithm to c++
+
 {
+
 	double rRange = r/255.0;
+
 	double gRange = g/255.0;
+
 	double bRange = b/255.0;
 
+
+
 	double cMax = std::max(std::max(rRange,gRange),bRange);
+
 	double cMin = std::min(std::min(rRange,gRange),bRange);
+
 	double delta = cMax - cMin;
 
+
+
 	int H;
+
 	double S;
+
 	double V;
 
+
+
 	if(cMax==rRange) // Hue calculation
+
 	{
+
 		H=60*fmod((gRange-bRange)/delta,6);
+
 	}
+
 	else if(cMax==gRange)
+
 	{
+
 		H=60*((bRange-rRange)/delta+2);
+
 	}
+
 	else if(cMax==bRange)
+
 	{
+
 		H=60*((rRange-gRange)/delta+4);
+
 	}
+
 	else
+
 	{
+
 		std::cout<<"Error in finding cMax comparison";
+
 	}
+
 	
+
 	if(cMax==0.0) // Saturation calculation
+
 	{
+
 		S=0;
+
 	}
+
 	else
+
 	{
+
 		S=delta/cMax;
+
 	}
+
+
 
 	V=cMax; // Value calculation
 
+
+
 	HSV hsvToReturn;
+
 	hsvToReturn.H = H;
+
 	hsvToReturn.S = S;
+
 	hsvToReturn.V = V;
 
+
+
 	return hsvToReturn; // Return HSV equivalent!
+
 }
+
+
 
 int isPixelWhite(int luminosity)
+
 {
+
 	if(luminosity > 240)
+
 	{
+
 		return 1; 
+
 	}
+
 	else 
+
 	{
+
 		return 0;
+
 	}
+
 }
+
+
 
 RGB getPixelRGB(ImagePPM currentFrame, int row, int column)
+
 {
+
 	RGB RGBToReturn;
+
 	RGBToReturn.r = get_pixel(currentFrame, row, column, 0); 
+
 	RGBToReturn.g = get_pixel(currentFrame, row, column, 1); 
+
 	RGBToReturn.b = get_pixel(currentFrame, row, column, 2); 
+
 	
+
 	return RGBToReturn;
+
 }
+
+
 
 bool isPixelRed(RGB pixel)
+
 {
+
 	if(pixel.r > 250 && pixel.g < 5 && pixel.b < 5)
+
 	{
+
 		return true;
+
 	}
+
 	else
+
 	{
+
 		return false;
+
 	}
+
 }
+
+
 
 bool isLeftSideRed(ImagePPM currentFrame)
+
 {
+
 	bool isRed = false;
+
 	int width = currentFrame.width / 2 - 1;
+
 	for(int i = 0; i < width; i++) // Width
+
 	{
+
 		for(int row = 0; row < currentFrame.height; row++) // Height
+
 		{
+
 			if(isPixelRed(getPixelRGB(currentFrame, row, i)))
+
 			{
+
 				isRed = true;
+
 			}
+
 		}
+
 	}
+
 	if(isRed)
+
 	{
+
 		return true;
+
 	}
+
 	else
+
 	{
+
 		return false;
+
 	}
+
 }
+
+
 
 bool isRightSideRed(ImagePPM currentFrame)
+
 {
+
 	bool isRed = false;
+
 	int width = currentFrame.width / 2 - 1;
+
 	for(int i = currentFrame.width - width; i < currentFrame.width; i++) // Width
+
 	{
+
 		for(int row = 0; row < currentFrame.height; row++) // Height
+
 		{
+
 			if(isPixelRed(getPixelRGB(currentFrame, row, i)))
+
 			{
+
 				isRed = true;
+
 			}
+
 		}
+
 	}
+
 	if(isRed)
+
 	{
+
 		return true;
+
 	}
+
 	else
+
 	{
+
 		return false;
+
 	}
+
 }
+
+
 
 bool isFrontSideRed(ImagePPM currentFrame)
+
 {
+
 	bool isRed = false;
+
 	int width = 30;
+
 	int height = 60;
+
 	for(int i = width; i < currentFrame.width-width; i++) // Width, doesn't check edge **width** pixels
+
 	{
+
 		for(int row = 0; row < height; row++) // Height
+
 		{
+
 			if(isPixelRed(getPixelRGB(currentFrame, row, i)))
+
 			{
+
 				isRed = true;
+
 			}
+
 		}
+
 	}
+
 	if(isRed)
+
 	{
+
 		return true;
+
 	}
+
 	else
+
 	{
+
 		return false;
+
 	}
+
 }
 
-int main(){
-    // Declare constants
-    const static int WADDLE_WIDTH = 50; 
-    const static int WADDLE_HEIGHT = 40;
-    const static int PIXEL_THRESHOLD = 5;
-    const static double ROBOT_SPEED = 16.5;
-    const static double TURN_MULTIPLIER = 0.25;
-	
-    if (initClientRobot() !=0){
-	std::cout<<" Error initializing robot"<<std::endl;
-    }
-    double vLeft = ROBOT_SPEED;
-    double vRight = ROBOT_SPEED;
-    bool turningLeft;
-    int leftRedPixels;
-    int rightRedPixels;
-    int middleRedPixels;
-    bool leftWhiteLine;
-
-    // Variable to store current frame in
-    ImagePPM currentFrame; 
-
-    while(1){
-		
-	    // Take picture
-		takePicture(); 
-	    // Save it as view.ppm
-		SavePPMFile("view.ppm",cameraView); 
-		// Open view.ppm, and save the contents in currentFrame variable
-		OpenPPMFile("view.ppm", currentFrame); 
-		
-		leftRedPixels = 0;
-		rightRedPixels = 0;
-		middleRedPixels = 0;
-		
-		int totalWhiteXValue = 0;
+int whiteLineLoop(int TURN_MULTIPLIER, int ROBOT_SPEED,ImagePPM currentFrame,double vLeft,double vRight){
+	int totalWhiteXValue = 0;
 		int totalWhitePixels = 0;
 		double averageWhiteXValue = 0;		
-
 		int middleWhitePixels = 0;
-	    
-	    // Check the entire frame for white pixels
-		for(int currentRow = 0; currentRow < currentFrame.height; currentRow++) 
-		{ 
+	 bool leftWhiteLine;
+	for(int currentRow = 0; currentRow < currentFrame.height; currentRow++) { 
 			int pixelGrayscale;
-			
 			for(int currentColumn = 0; currentColumn < currentFrame.width; currentColumn++) 
 			{
 				pixelGrayscale = get_pixel(currentFrame, currentRow, currentColumn, 3); 
@@ -240,8 +384,7 @@ int main(){
 		
 		// Calculate average X location of white pixels
 		averageWhiteXValue = (double)totalWhiteXValue / (double)totalWhitePixels ;
-		if(!leftWhiteLine)
-		{
+		if(!leftWhiteLine) {
 			// Check there are actually pixels, otherwise the robot does not do anything relating to the white line
 			if(totalWhitePixels > 0)
 			{
@@ -266,16 +409,114 @@ int main(){
 					vRight =  -(averageWhiteXValue - (currentFrame.width / 2)) * TURN_MULTIPLIER;
 					setMotors(vLeft,vRight);					
 				}
-	   
 			}
 			else
 			{
 				leftWhiteLine = true;
 			}
+			
 		}
-	    else // There is no white line, we do this instead of white line logic
-	    {
-	    
+	return totalWhitePixels;
+}
+
+void emergencyStuckManuveours(){
+	std::cout<<"robot is stuck"<<std::endl;
+	setMotors(-20.0,-20.0);
+	usleep(500000);
+	setMotors(-5.0,5.0);
+	while(isLeftSideRed(cameraView)==false){
+		takePicture();
+		usleep(17500);
+	}
+}
+
+void stuckFunction(){
+	ImagePPM currentFrame;
+	std::vector<int> vec1;
+	std::vector<int> vec2;
+	int matchescounter = 0;
+	OpenPPMFile("view1.ppm", currentFrame);
+	
+	for(int i = 0;i<currentFrame.width-1;i=i+5){
+		for(int j = 0;j<currentFrame.height-1;j=j+5){
+			vec1.push_back(get_pixel(currentFrame,j,i,1));
+		}
+	}
+	OpenPPMFile("view2.ppm", currentFrame);
+	for(int i = 0;i<currentFrame.width-1;i=i+5){
+		for(int j = 0;j<currentFrame.height-1;j=j+5){
+			vec2.push_back(get_pixel(currentFrame,j,i,1));
+		}
+	}
+	for(int i = 0;i<vec1.size();i++){
+		if(vec1[i] == vec2[i]){
+			matchescounter++;
+		}
+	}
+	if(matchescounter>vec1.size()*0.4){
+		std::cout<<"robot is stuck------------------------------------------"<<std::endl;
+		emergencyStuckManuveours();
+	}
+}
+
+
+int main(){
+    // Declare constants
+    const static int WADDLE_WIDTH = 50; 
+    const static int WADDLE_HEIGHT = 40;
+    const static int PIXEL_THRESHOLD = 5;
+    const static double ROBOT_SPEED = 16.5;
+    const static double TURN_MULTIPLIER = 0.25;
+    const static float TURN_RATIO = 0.4;
+    if (initClientRobot() !=0){
+	std::cout<<" Error initializing robot"<<std::endl;
+    }
+    double vLeft = ROBOT_SPEED;
+    double vRight = ROBOT_SPEED;
+    bool turningLeft;
+    int leftRedPixels;
+    int rightRedPixels;
+    int middleRedPixels;
+
+	bool whatimage = true; //code needs two images to check if the robot gets stuck. this will be used to alternate the image being used
+	int dostuckcheck;
+    // Variable to store current frame in
+    ImagePPM currentFrame; 
+    
+    int whitepixels;
+    do{//this loop follows the white line movement style until it runs out of white line. then the loop finishes and its inside the red maze so the main loop can run
+		// Take picture
+		takePicture(); 
+	    // Save it as view.ppm
+		SavePPMFile("view1.ppm",cameraView); 
+		// Open view.ppm, and save the contents in currentFrame variable
+		OpenPPMFile("view1.ppm", currentFrame); 
+		whitepixels = whiteLineLoop(TURN_MULTIPLIER, ROBOT_SPEED,currentFrame,vLeft,vRight);
+		}while(whitepixels>0);
+	
+	
+    while(1){
+	    // Take picture
+		takePicture(); 
+	    // Save it as view.ppm
+	    std::cout<<"whatimage: "<<whatimage<<std::endl;
+	    if(whatimage){
+		SavePPMFile("view1.ppm",cameraView); 
+		}
+		else{
+			SavePPMFile("view2.ppm",cameraView); 
+		}
+		// Open view.ppm, and save the contents in currentFrame variable
+		if(whatimage){
+		OpenPPMFile("view1.ppm", currentFrame); 
+		}
+		else{
+			OpenPPMFile("view2.ppm", currentFrame);
+		}
+		leftRedPixels = 0;
+		rightRedPixels = 0;
+		middleRedPixels = 0;
+		
 			// Robot Algorithm:
 			// If there is something infront, turn around
 			// Then, if there is a wall to the left:
@@ -283,55 +524,54 @@ int main(){
 			//		If there isn't, move forward
 			// Then, if there is no wall to the left,
 			// Begin turning left
-			
-			
 			// Now we must check if there is red right infront of the robot
 			// If there is, we REVERSE before doing anything else
-			
 			RGB center1 = getPixelRGB(currentFrame, currentFrame.height-20, 70); // 70 and 80 are 5 pixels away from the center width of the image
+
 			RGB center2 = getPixelRGB(currentFrame, currentFrame.height-20, 80);
-			
 			if(isPixelRed(center1) || isPixelRed(center2)) // Is there a barricade RIGHT infront of us?
 			{
 				if(isLeftSideRed(currentFrame)==true)
 				{
 					// Wall to our left, turn right while reversing
 					setMotors(ROBOT_SPEED, -ROBOT_SPEED); // One of the center pixels was red, there's a barricade infront of us
-					std::cout<<"Barricade RIGHT infront of us, reversing right on the spot.";
+					std::cout<<"Barricade RIGHT infront of us, reversing right on the spot."<<std::endl;
 				}
 				else
 				{
 					// Turn left while reversing
 					setMotors(-ROBOT_SPEED, ROBOT_SPEED); // One of the center pixels was red, there's a barricade infront of us
-					std::cout<<"Barricade RIGHT infront of us, reversing left on the spot.";					
+					std::cout<<"Barricade RIGHT infront of us, reversing left on the spot."<<std::endl;					
 				}
-
 			}
 			else if(isLeftSideRed(currentFrame)==true)
 			{
 				// Left side IS red
 				if(isFrontSideRed(currentFrame)==true) // Front side red, barricade infront of us so we turn right
 				{
-					setMotors(ROBOT_SPEED, ROBOT_SPEED*0.4f); // Turn right
-					std::cout<<"Wall left, barricade infront, turning right.";
+					setMotors(ROBOT_SPEED, ROBOT_SPEED*TURN_RATIO); // Turn right
+					std::cout<<"Wall left, barricade infront, turning right."<<std::endl;
 				}
 				else
 				{
 					setMotors(ROBOT_SPEED, ROBOT_SPEED); // Continue forward
-					std::cout<<"Wall left, going forward.";
+					std::cout<<"Wall left, going forward."<<std::endl;
 				}
 			}
 			else // There is no wall to the left of us
 			{
-				setMotors(ROBOT_SPEED*0.4f, ROBOT_SPEED); // Turn left
-				std::cout<<"No wall left, turning left.";
+				setMotors(ROBOT_SPEED*TURN_RATIO, ROBOT_SPEED); // Turn left
+				std::cout<<"No wall left, turning left."<<std::endl;
 			}
+		dostuckcheck = rand() % 5 + 1;
+		if(dostuckcheck == 1){
+			std::cout<<"checking if stuck+++++++++++++++++++++++++++++++++++++++++++++++++++++"<<std::endl;
+			stuckFunction();//function both checks if robot is stuck and responds if it is
 		}
-
+		whatimage = !whatimage;//changes whatimage for next loop repetition
 		//std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
 		//std::cout<<"Left="<<leftWhitePixels<<" Right="<<rightWhitePixels<<std::endl;
 		usleep(17500);
 	}
-
 }
 
